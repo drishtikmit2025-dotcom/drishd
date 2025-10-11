020d893eb6c71877be96345494a15cc831a963f8
jay-adapa <adapajayakrishna004@gmail.com>
2025-10-11 17:39:21 +0530

    Allow render domian

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,8 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ['.onrender.com', 'localhost'],
+
     fs: {
       allow: [
         path.resolve(__dirname, "."),
